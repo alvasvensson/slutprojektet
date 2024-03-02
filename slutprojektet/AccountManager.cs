f993132fd7e8952a5e0e2c81c70e51faa418f793
Alva Svensson <alva.svensson@elev.ga.ntig.se>
2024-03-02 12:22:35 +0100

    2/3

--- slutprojektet/AccountManager.cs
+++ slutprojektet/AccountManager.cs
@@ -5,17 +5,50 @@ public class AccountManager
     List<Account> accounts = new List<Account>();
     //Läsa in JSOn filen och lägga in alla objekt i den till accountslistan
 
-    public void LogIn()
+    public bool LogIn()
     {
+        bool userExists = false;
+        int chosenAccount = -1;
         Console.WriteLine("Username?");
+        string inputUN = Console.ReadLine();
         //checka om det användarnamnet finns
-        foreach (Account a in accounts)
+        for (int a = 0; a < accounts.Count; a++)
         {
+            if (inputUN == accounts[a].Username)
+            {
+                userExists = true;
+                chosenAccount = a;
+                break;
+            }
+
+            else
+            {
+                userExists = false;
+            }
+        }
+        if (userExists == true)
+        {
+            Console.WriteLine("password?");
+            // kolla om lösenordet matcher det angivna användarnamnets lösenord
+            string inputPW = Console.ReadLine();
+            if (inputPW == accounts[chosenAccount].Password)
+            {
+                Console.WriteLine($"Welcome back {accounts[chosenAccount].Username}!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
 
         }
+        else
+        {
+            Console.WriteLine("We couldn't find a user by that name");
+            return false;
+        }
 
-        Console.WriteLine("password?");
-        // kolla om lösenordet matcher det angivna användarnamnets lösenord
 
 
     }
